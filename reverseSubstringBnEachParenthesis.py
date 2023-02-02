d236f64108856c5a1b4fccc3c1803cc355ac41d1
Yohannes1556 <61546367+Yohannes1556@users.noreply.github.com>
2023-02-02 23:49:28 +0300

    Create reverseSubstringBnEachParenthesis.py

---
+++ reverseSubstringBnEachParenthesis.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        i = 0
+        while (i<len(s)):
+            if s[i] != ')':
+                stack.append(s[i])
+            else:
+                stackPoppedEle = []
+                while(stack[-1] != '('):
+                    stackPoppedEle.append(stack.pop())
+                stack.pop()
+                stack.extend(stackPoppedEle)
+            i+=1
+        return "".join(stack)
