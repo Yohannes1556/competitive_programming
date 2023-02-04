df186db8fb79cac8b3c3cc9baf5cb12fe4363eaf
Yohannes1556 <61546367+Yohannes1556@users.noreply.github.com>
2023-02-04 15:11:39 +0300

    Create findKthBit1545.py

---
+++ findKthBit1545.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        def g(s):
+            arr = []
+            for x in s:
+                if x == "0":
+                    arr.append("1")
+                else:
+                    arr.append("0")
+            return "".join(reversed(arr))
+        s = "0"
+        for _ in range(n):
+            s = s + "1" +g(s)
+        return s[k-1]
